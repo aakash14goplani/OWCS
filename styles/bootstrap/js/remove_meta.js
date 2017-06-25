04a55a6618fdaff691126b923078f81c5c7914a5
aakash14goplani <goplaniaakash14@gmail.com>
2017-06-26 02:17:36 +0530

    UI Changed

--- styles/bootstrap/js/remove_meta.js
+++ styles/bootstrap/js/remove_meta.js
@@ -1,4 +1,8 @@
-<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-$(document).ready(function() {
-    $('.gist-meta').hide();
-});
+// hide meta-data
+var divsToHide = document.getElementsByClassName("gist-meta");
+for(var i = 0; i < divsToHide.length; i++)
+    divsToHide[i].style.display = "none";
+// hide border
+var bordersToHide = document.getElementsByClassName("gist-file");
+for(var i = 0; i < bordersToHide.length; i++)
+    bordersToHide[i].style.border = "none";
