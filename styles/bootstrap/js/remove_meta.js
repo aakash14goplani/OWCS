1b840b59c69961f7cd03bcce9303d288bf9a9015
Goplani <aakash.goplani@accenture.com>
2017-12-05 20:01:23 +0530

    updated remove_meta.js file to display gist filename on meta tags

--- styles/bootstrap/js/remove_meta.js
+++ styles/bootstrap/js/remove_meta.js
@@ -6,10 +6,18 @@ document.querySelector("head").appendChild(jq);
 function proceed () {
     $("a > img").unwrap();
 }
-// hide meta-data
+// hide meta-data and display filename at meta section
 var divsToHide = document.getElementsByClassName("gist-meta");
-for(var i = 0; i < divsToHide.length; i++)
+var fileName = "";
+for(var i = 0; i < divsToHide.length; i++){
     divsToHide[i].style.display = "none";
+    divsToHide[i].style.display = "none";
+    fileName = document.getElementsByClassName("gist-meta")[i].children[1];
+    var meta = document.createElement('meta');
+    meta.name = "gist-file-" + i;
+    meta.content = fileName;
+    document.getElementsByTagName('head')[0].appendChild(meta);
+}
 // hide border
 var bordersToHide = document.getElementsByClassName("gist-file");
 for(var i = 0; i < bordersToHide.length; i++)
